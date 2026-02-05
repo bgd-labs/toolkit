a2d13ebc400f026d64d821fb42e70db5f1836f34
Lukas <ls@bgdlabs.com>
2026-02-05 20:59:15 +0100

    fix: browser build (#6)

--- packages/toolbox/tsdown.config.ts
+++ packages/toolbox/tsdown.config.ts
@@ -1,15 +1,30 @@
 import { defineConfig } from "tsdown";
 
-export default defineConfig({
-  exports: true,
-  sourcemap: true,
-  entry: {
-    index: "src/index.ts",
-    browser: "src/browser.ts"
+export default defineConfig([
+  {
+    exports: true,
+    sourcemap: true,
+    entry: {
+      index: "src/index.ts",
+    },
+    platform: "node",
+    format: ["esm"],
+    dts: {
+      resolve: true,
+    },
+    clean: true,
   },
-  format: ["esm"],
-  dts: {
-    resolve: true
+  {
+    exports: true,
+    sourcemap: true,
+    entry: {
+      browser: "src/browser.ts",
+    },
+    platform: "browser",
+    format: ["esm"],
+    dts: {
+      resolve: true,
+    },
+    clean: true,
   },
-  clean: true,
-});
+]);
